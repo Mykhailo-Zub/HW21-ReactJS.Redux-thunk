b3221ef2075903c7bbd5388181474c88e3a8e5f3
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-10-07 00:15:10 +0300

    some fix

--- src/components/AddTweet.js
+++ src/components/AddTweet.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import { Redirect } from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import { Redirect } from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import SendIcon from "@material-ui/icons/Send";
 import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
