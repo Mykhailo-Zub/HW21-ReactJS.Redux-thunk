ee6fbdf0fe6190f49d3e324a4762d8563e163c14
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-10-06 18:40:43 +0300

    Updates

---
+++ precache-manifest.0eed3e0384d034060714594e3ed0fe5f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f948eb35c1c646872e3c17dd0bee5ad1",
+    "url": "/HW21-ReactJS.Redux-thunk/index.html"
+  },
+  {
+    "revision": "1d74580b5df744b8d67e",
+    "url": "/HW21-ReactJS.Redux-thunk/static/css/main.fd1ff3ce.chunk.css"
+  },
+  {
+    "revision": "c70a6f52b9375f4b858e",
+    "url": "/HW21-ReactJS.Redux-thunk/static/js/2.fc644308.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HW21-ReactJS.Redux-thunk/static/js/2.fc644308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d74580b5df744b8d67e",
+    "url": "/HW21-ReactJS.Redux-thunk/static/js/main.1c1d05e5.chunk.js"
+  },
+  {
+    "revision": "3ed92c7f6a2837125e8d",
+    "url": "/HW21-ReactJS.Redux-thunk/static/js/runtime-main.d6179335.js"
+  },
+  {
+    "revision": "0002e5f6fa980eed02d3897ecc5eb36d",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/comment.0002e5f6.svg"
+  },
+  {
+    "revision": "e371e0b38f5eb7a65737f412a81f0db4",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/comment_active.e371e0b3.svg"
+  },
+  {
+    "revision": "39726703564a2a71e8077948ac5d964c",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/confirm.39726703.svg"
+  },
+  {
+    "revision": "da3338122f87907c1fb994f17002f866",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/down-arrow.da333812.svg"
+  },
+  {
+    "revision": "12220a2259c8f1718847197c4bfdb84e",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/like.12220a22.svg"
+  },
+  {
+    "revision": "f8fb9825e43cd716ad637aa42aae3994",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/like_active.f8fb9825.svg"
+  },
+  {
+    "revision": "444db2463624b56ae55674c6e654d9b9",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/share.444db246.svg"
+  },
+  {
+    "revision": "abbcc3bb79d684270cdfb4d788fc558d",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/share_active.abbcc3bb.svg"
+  },
+  {
+    "revision": "8f047106898ba92d2dc7e03f29f16bed",
+    "url": "/HW21-ReactJS.Redux-thunk/static/media/upload.8f047106.svg"
+  }
+]);
